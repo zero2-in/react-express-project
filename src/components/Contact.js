e49500affe81c918f5d93e33e5db7e2d5d532dc6
zero2-in <wowin21@gmail.com>
2022-03-28 17:28:34 +0900

    검색 기능 구현
    onChange 이벤트 추가 후 filter 함수를 통해 검색

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,7 @@ export default class Contact extends React.Component {
         super(props);
 
         this.state = {
+            keyword: '',
             contactData: [{
                 name: 'Abet',
                 phone: '010-0000-0000'
@@ -20,10 +21,24 @@ export default class Contact extends React.Component {
                 phone: '010-0000-0004'
             }]
         };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            keyword: e.target.value
+        });
     }
 
     render() {
         const mapToComponents = (data) => {
+            data.sort();
+            data = data.filter(
+                (contact) => {
+                    return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
+                }
+            );
             return data.map((contact, i) => {
                 return (<ContactInfo contact={contact} key={i}/>);
             });
@@ -32,6 +47,12 @@ export default class Contact extends React.Component {
         return (
             <div>
                 <h1>Contacts</h1>
+                <input 
+                    name="keyword" 
+                    placeholder="Search" 
+                    value={this.state.keyword} 
+                    onChange={this.handleChange}
+                />
                 <div>
                     {mapToComponents(this.state.contactData)}
                 </div>
