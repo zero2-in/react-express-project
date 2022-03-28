ea6d6c78eb60337b5a15a4f37bfea4cf5d6b0f41
zero2-in <wowin21@gmail.com>
2022-03-28 17:11:32 +0900

    Contact 정보 추가
    ContactInfo, Contact 컴포넌트 추가함.
    ContactInfo는 이름과 전화번호를 출력하는 틀이고
    Contact는 state에 데이터를 추가하여 ContactInfo에 맵핑,
    App 컴포넌트에 Contact 컴포넌트로 추가함

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Contact from "./Contact";
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello</h1>
+            <Contact/>
         );
     }
 }
