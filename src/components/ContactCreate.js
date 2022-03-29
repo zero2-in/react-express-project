bfcec3af3d82ed24185f99bfd3d9537ce15041ab
zero2-in <wowin21@gmail.com>
2022-03-29 14:21:54 +0900

    Contact 엑스트라 기능 구현
    KeyPress로 버튼을 클릭하지 않고도 Enter 키코드로 저장 및 수정가능.
    ref속성으로 Create Contact 후 자동인풋 포커스

--- src/components/ContactCreate.js
+++ src/components/ContactCreate.js
@@ -11,6 +11,7 @@ export default class ContactCreate extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     handleChange(e) {
@@ -31,6 +32,14 @@ export default class ContactCreate extends React.Component {
             name: '',
             phone: ''
         });
+
+        this.nameInput.focus();
+    }
+
+    handleKeyPress(e) {
+        if(e.charCode === 13) {
+            this.handleClick();
+        }
     }
 
     render() {
@@ -44,6 +53,7 @@ export default class ContactCreate extends React.Component {
                         placeholder="name" 
                         value={this.state.name} 
                         onChange={this.handleChange}
+                        ref={(ref) => {this.nameInput = ref}}
                     />
                     <input 
                         type="text" 
@@ -51,6 +61,7 @@ export default class ContactCreate extends React.Component {
                         placeholder="phone" 
                         value={this.state.phone} 
                         onChange={this.handleChange}
+                        onKeyPress={this.handleKeyPress}
                     />
                 </p>
                 <button onClick={this.handleClick}>Create</button>
