fab7805a7728ae7517d0d67b09609b41a53beff1
zero2-in <wowin21@gmail.com>
2022-03-29 13:05:09 +0900

    Contact 추가/삭제/수정 기능 구현
    Immutability Helper를 이용한
    $set, $splice, $push 예제

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ContactInfo from "./ContactInfo";
 import ContactDetails from "./ContactDetails";
+import update from "react-addons-update";
 
 export default class Contact extends React.Component {
     constructor(props) {
@@ -26,6 +27,10 @@ export default class Contact extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
     }
 
     handleChange(e) {
@@ -40,6 +45,32 @@ export default class Contact extends React.Component {
         });
 
         console.log(key, 'is selected');
+    }
+
+    handleCreate(contact) {
+        this.setState({
+            contactData: update(this.state.contactData, { $push: [contact] })
+        });
+    }
+
+    handleRemove() {
+        this.setState({
+            contactData: update(this.state.contactData, { $splice: [[this.state.selectedKey, 1]]}),
+            selectedKey: -1
+        });
+    }
+
+    handleEdit(name, phone) {
+        this.setState({
+            contactData: update(this.state.contactData, 
+                {
+                    [this.state.selectedKey]: {
+                        name: { $set: name },
+                        phone: { $set: phone }
+                    }
+                }    
+            )
+        })
     }
 
     render() {
